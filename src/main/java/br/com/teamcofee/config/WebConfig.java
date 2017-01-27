97659f9d9d2ad51753bbf22a42ab5d6d7a714329
josivan <josivanps@gmail.com>
2017-01-27 16:41:39 -0200

    One get and one post.

--- src/main/java/br/com/teamcofee/config/WebConfig.java
+++ src/main/java/br/com/teamcofee/config/WebConfig.java
@@ -1,6 +1,9 @@
 package br.com.teamcofee.config;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
+
+import spark.Response;
 
 public class WebConfig {
 
@@ -9,6 +12,22 @@ public class WebConfig {
   }
   
   private void setupRoutes() {
+    setupGets();
+    setupPosts();
+  }
+  
+  private void setupGets() {
     get("/hello", (req, res) -> "Hello Team Coffee");
+    get("/user", (req, res) -> {
+      return "{[{name: \'user\'},{name: \'user2\'}]}";
+    });
+  }
+  
+  private void setupPosts() {
+    post("/user", (req, res) -> {
+      System.out.println("Body: " + req.body());
+      res.type("application/json");
+      return "{\"id\": \"789\"}";
+    });
   }
 }
