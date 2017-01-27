a99e4d24ec2ce402b64ab38ff808268b6d690c14
josivan <josivanps@gmail.com>
2017-01-27 17:18:57 -0200

    Spring is running in web mode too.

--- src/main/java/br/com/teamcofee/rest/TeamCoffeeServer.java
+++ src/main/java/br/com/teamcofee/rest/TeamCoffeeServer.java
@@ -1,12 +1,19 @@
 package br.com.teamcofee.rest;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import br.com.teamcofee.config.WebConfig;
+import br.com.teamcofee.service.TeamCoffeeService;
 import spark.servlet.SparkApplication;
 
 public class TeamCoffeeServer implements SparkApplication {
 
   @Override
   public void init() {
-    new TeamCofeeApp();
+    System.out.println("Starting SparkApplication...");
+    AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(TeamCofeeApp.class);
+    new WebConfig(ctx.getBean(TeamCoffeeService.class));
+    ctx.registerShutdownHook();
   }
 
 }
