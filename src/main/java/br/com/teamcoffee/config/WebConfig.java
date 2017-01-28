64f1230a4cfa31927adb1796c1cd0d7a212c857e
josivan <josivanps@gmail.com>
2017-01-28 00:59:50 -0200

    Using gson and setting email as unique.

--- src/main/java/br/com/teamcoffee/config/WebConfig.java
+++ src/main/java/br/com/teamcoffee/config/WebConfig.java
@@ -3,16 +3,14 @@ package br.com.teamcoffee.config;
 import static spark.Spark.get;
 import static spark.Spark.post;
 
-import java.util.Map;
-
-import com.mongodb.util.JSON;
+import com.google.gson.Gson;
 
 import br.com.teamcoffee.domain.User;
 import br.com.teamcoffee.service.TeamCoffeeService;
-import br.com.teamcoffee.util.TeamCoffeeUtils;
 
 public class WebConfig {
   private TeamCoffeeService service;
+  private Gson gson = new Gson();
 
   public WebConfig(TeamCoffeeService service) {
     this.service = service;
@@ -33,17 +31,9 @@ public class WebConfig {
   
   private void setupPosts() {
     post("/user", (req, res) -> {
-      User user = createUser(TeamCoffeeUtils.bodyRequestToMap(req));
+      User user = gson.fromJson(req.body(), User.class);
       this.service.save(user);
-      res.type("application/json");
-      return String.format("{\"id\": %s }", user.id); 
-    });
-  }
-  
-  private User createUser(Map<String, String> data) {
-    User user = new User();
-    user.name = data.get("name");
-    user.email = data.get("email");
-    return user;
+      return user; 
+    }, gson::toJson);
   }
 }
--- src/main/java/br/com/teamcoffee/domain/User.java
+++ src/main/java/br/com/teamcoffee/domain/User.java
@@ -1,11 +1,14 @@
 package br.com.teamcoffee.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document
 public class User {
   @Id public String id;
+  @Indexed(unique = true) public String email;
   public String name;
-  public String email;
   
   @Override
   public String toString() {
